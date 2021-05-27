68ee5800511ae5f64b86a79e7a6aad3c9c0e811b
Verrem98 <emielverhoef@hotmail.com>
2021-05-27 13:28:02 +0200

    Verbindingen tussen plaatsen toegevoegd

--- src/com/company/Plaats.java
+++ src/com/company/Plaats.java
@@ -1,4 +1,53 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Plaats {
+
+    private String naam;
+
+    private HashMap<Plaats, Double> vluchtVerbindingen = new HashMap<>();
+    private HashMap<Plaats, Double> treinritVerbindingen = new HashMap<>();
+    private HashMap<Plaats, Double> ritVerbindingen = new HashMap<>();
+
+
+
+    public Plaats(String naam){
+
+        this.naam = naam;
+
+    }
+
+
+    public void setVerbinding(Plaats plaats, double waarde, String verbindingType){
+
+        if(verbindingType.equals("vlucht")){
+            this.vluchtVerbindingen.put(plaats,waarde);
+
+        }
+        if(verbindingType.equals("treinrit")){
+            this.treinritVerbindingen.put(plaats,waarde);
+
+
+        }
+        if(verbindingType.equals("rit")){
+            this.ritVerbindingen.put(plaats,waarde);
+
+
+        }
+
+
+    }
+
+    public HashMap<Plaats, Double> getVluchtVerbindingen() {
+        return vluchtVerbindingen;
+    }
+
+    public HashMap<Plaats, Double> getTreinVerbindingen() {
+        return treinritVerbindingen;
+    }
+
+    public HashMap<Plaats, Double> getWegVerbindingen() {
+        return ritVerbindingen;
+    }
 }
--- src/com/company/Reis.java
+++ src/com/company/Reis.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Reis {
+
+
+
+
+    public void planReis(Plaats startPlaats, Plaats eindPlaats, String methode){
+
+        System.out.println(startPlaats.getVluchtVerbindingen());
+
+    }
 }
